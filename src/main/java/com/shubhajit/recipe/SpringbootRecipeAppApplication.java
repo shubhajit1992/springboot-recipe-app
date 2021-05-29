b03a2ba3165a548b59267f29f7bf83cf72f6912d
Shubhajit Sahoo <shubhajit.sahoo@gmail.com>
2021-05-29 15:55:07 +0530

    Initial commit

---
+++ src/main/java/com/shubhajit/recipe/SpringbootRecipeAppApplication.java
@@ -0,0 +1,13 @@
+package com.shubhajit.recipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRecipeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRecipeAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shubhajit/recipe/SpringbootRecipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shubhajit.recipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRecipeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
