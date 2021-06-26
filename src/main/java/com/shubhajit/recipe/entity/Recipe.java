dcc6fa00ab45887e3ceb6f57b4b7e2fa907355ba
Shubhajit Sahoo <shubhajit.sahoo@gmail.com>
2021-06-26 19:16:52 +0530

    Adding Many to One JPA Example

--- src/main/java/com/shubhajit/recipe/entity/Recipe.java
+++ src/main/java/com/shubhajit/recipe/entity/Recipe.java
@@ -1,10 +1,14 @@
 package com.shubhajit.recipe.entity;
 
+import java.util.Set;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Lob;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Recipe {
@@ -21,6 +25,9 @@ public class Recipe {
     private String directions;
     // todo add
     // private Difficulty difficulty;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+    private Set<Ingredient> ingredients;
 
     @Lob
     private Byte[] image;
@@ -96,4 +103,13 @@ public class Recipe {
     public void setImage(Byte[] image) {
         this.image = image;
     }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+
 }
